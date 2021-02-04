fe64d6a829a1a26a2296b60b3ac2df32e1c28a65
Friedrich Romstedt <friedrichromstedt@gmail.com>
2021-02-04 08:42:03 +0100

    Slight improvement to the test program [2021-02-02_1054.py]
    There now is an explanatory comment included.

--- test/2021-02-02_1054.py
+++ test/2021-02-02_1054.py
@@ -2,5 +2,8 @@ from bughuntingfr.model import Image
 import numpy
 
 im = Image(2, 3)
+
 print("I make it succeed")
+    # When this statememt is removed, the program segfaults.
+
 print(numpy.asarray(im))
